4ec37d5a7f3511defa739670885cbcd7aad76a30
Rhiannon Smeby <smebyrhi@gmail.com>
2020-12-17 14:05:20 -0600

    build

---
+++ build/precache-manifest.ecd2aa5cf220a65b86ab79cacff0f6eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb4a40b75335c1c99808db3b29df67f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2710a6213afb71647056",
+    "url": "/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "91be17f00bf027269ee1",
+    "url": "/static/js/2.a3f18fbf.chunk.js"
+  },
+  {
+    "revision": "2710a6213afb71647056",
+    "url": "/static/js/main.8349d261.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
